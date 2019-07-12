5a1c1f457445420f7a652f5817ca075d8e44fb7f
Latif Kabir <siplukabir@gmail.com>
2019-07-12 17:20:14 -0400

    Updated

--- analysis/src/FmsQA.C
+++ analysis/src/FmsQA.C
@@ -4,7 +4,7 @@ void FmsQA()
     TFile *f = new TFile("FmsQA.root","recreate"); //Creates a root file that will hold the histograms.
 	
     StChain *chain = new StChain;
-    StMuDstMaker *mMaker = new StMuDstMaker(0,0,"","resources/temp/FmsFileList.list",""); //Opens the STAR data to be used.
+    StMuDstMaker *mMaker = new StMuDstMaker(0,0,"","resources/temp/FmsFileList.list","", 10000); //Opens the STAR data to be used.
     StMuDst *mDst =  mMaker->muDst();  //Get the Tree from MuDst.
     TChain *tree = mMaker->chain();
 	
@@ -16,32 +16,57 @@ void FmsQA()
     int iEvent = 0; // Event number.
     int channel; 
     int detID;
-    const oMaxCh = 600; 
-    TH1F *adcDist[oMaxCh]; //Initilize the 1D histogram.
+    const oMaxCh = 571; 
+    const iMaxCh = 288;
+    TH1F *adcDist[4][oMaxCh]; //Initilize the 1D histogram.
     
-    for (int l = 0; l < oMaxCh; l++) //Use a for loop to create histograms for each channel.
+    for(int i = 0; i < 4; ++i)
     {
-	TString title = "adcDist";
-	title += l;        
-	adcDist[l] = new TH1F(title,title,300,0.0,500); //Creating 1D histograms for each channel.
-    }   
-
+	int MaxCh;
+	if(i == 0 || i == 1)
+	    MaxCh = oMaxCh;
+	else
+	    MaxCh = iMaxCh;
+	for (int l = 0; l < MaxCh; l++) 
+	{
+	    TString title = "adcDist:";
+	    title += (i + 8);        
+	    title += "-";        
+	    title += l;        
+	    adcDist[i][l] = new TH1F(title, title, 300, 0.0, 500); //Creating 1D histograms for each channel.
+	}   
+    }
+    int maxx[4] = {0};    
     while(br->GetEntry(iEvent++))
     {
 	int nHits = array->GetEntriesFast();
+
 	for(int j = 0; j < nHits; j++) //For loop to fill each channel histogram.
 	{	     
 	    hit = (StMuFmsHit*) array->At(j); //Look at documentaion for hit.
 	    detID = hit->detectorId();        //Gets detector ID for each event.
 	    channel = hit->channel();         //Gets channel for each event. 288 for small/oMaxCh for large.
-	    if (detID == 8)                   //Only take data from detector 8.
-		adcDist[channel]->Fill(hit->adc()); //fill the histogram with adc data.
-	} 
+	    if(detID >= 8 && detID <= 11)     // Exclude detector IDs corresponding to FPOST, FPS, FPD etc.
+	    {
+		adcDist[detID - 8][channel - 1]->Fill(hit->adc()); //fill the histogram with adc data. Note: channel number starts from 1.
+		if(channel > maxx[detID - 8])
+		    maxx[detID - 8] = channel;
+	    }
+	}
     }
-    f->cd();
-    for(int k = 0; k < oMaxCh; k++)
-	adcDist[k]->Write(); //write each histograms.
 
+    f->cd();
+    
+    for(int i = 0; i < 4; ++i)
+    {
+	int MaxCh;
+	if(i == 0 || i == 1)
+	    MaxCh = oMaxCh;
+	else
+	    MaxCh = iMaxCh;
+	for (int l = 0; l < MaxCh; l++) 	
+	    adcDist[i][l]->Write();	   
+    }
     f->Close();    
 }			
 	
