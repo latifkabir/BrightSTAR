cb5e862583a90c1c5d34b2bb42e4e2aef7bf0a26
Latif Kabir <siplukabir@gmail.com>
2019-11-20 18:50:11 -0500

    Updated

--- scripts/AnFmsRp/RemoveBadFiles.C
+++ scripts/AnFmsRp/RemoveBadFiles.C
@@ -11,12 +11,11 @@
 
 using namespace std;
 
-
 void RemoveBadFiles()
 {
     string basePath = "/star/u/kabir/GIT/BrightSTAR/jobResults/condor/";
     ifstream inFile("/star/u/kabir/GIT/BrightSTAR/resources/temp/temp.list");
-    ofstream zFile("Run15ZombieFileList.txt");    
+    ofstream zFile("Run15ZombieFileListBackup.txt");    
     if(!inFile || !zFile)
     {
 	cout << "Unable to read input file or write outout file" <<endl;
@@ -28,6 +27,8 @@ void RemoveBadFiles()
     Int_t run;
     string runStr;
     Int_t fileCount = 0;
+    Int_t numBadFiles = 0;
+    string command;
     while(!inFile.eof())
     {
 	inFile >> dataFileName;
@@ -44,14 +45,20 @@ void RemoveBadFiles()
 	    TFile *f = new TFile(fileName.c_str());
 	    if(f->IsZombie())
 	    {
+		cout << "Outfile: "<< fileName <<endl;
 		cout << "----------------> Zombie file:"<< dataFileName <<endl;
 		zFile << dataFileName <<endl;
+		command = ".! rm " + fileName;
+		//cout << command <<endl;
+		//gROOT->ProcessLine(command.c_str());
+		++numBadFiles;
 	    }
 	    f->Close();
 	    delete f;
 	}
 	++fileCount;
-	if(fileCount > 20)
-	    break;
     }
+
+    cout << "Total files:" << fileCount <<endl;
+    cout << "Unablailable files:"<< numBadFiles <<endl;
 }
