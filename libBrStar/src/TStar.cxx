e9b718d5574acbdc34bebac60b28667e660fa7e5
Latif Kabir <siplukabir@gmail.com>
2020-05-09 18:10:06 -0400

    updated TStar class

--- libBrStar/src/TStar.cxx
+++ libBrStar/src/TStar.cxx
@@ -16,6 +16,7 @@
 #include "TSystem.h"
 #include "TClass.h"
 #include "TDictionary.h"
+#include "TBrowser.h"
 #include "TStar.h"
 
 #include<sstream>
@@ -70,106 +71,7 @@ Bool_t TStar::IsValid(TString filePath)
     return kTRUE;
 }
 
-
-Int_t TStar::GetCounter()
-{
-    TString filePath = Config->GetFilePath();
-    filePath += "FileCounter.txt";    
-    fstream counterFile(filePath,ios::in | ios::out);
-    Int_t fileNo = -1;
-    if(!counterFile)
-    {
-	cout << "WARNING: The vault file counter NOT found" <<endl;
-	return -1;
-    }
-
-    counterFile>>fileNo;	
-    ++fileNo;
-    counterFile.seekg(0,ios::beg);
-    counterFile<<fileNo;
-    counterFile.close();
-
-    TString rootFilePath = Config->GetFilePath() + "root/";
-    rootFilePath += fileNo;
-    rootFilePath += ".root";
-
-    if(!gSystem->AccessPathName(rootFilePath))
-    {
-	cout << "WARNING: The vault-file already exists. Overwriting NOT permitted." <<endl;
-	return -1;
-    }
-    return fileNo;
-}
-
-void TStar::SetCounter(Int_t count)
-{
-    TString filePath = Config->GetFilePath();
-    filePath += "FileCounter.txt";    
-    fstream counterFile(filePath, ios::out);
-    Int_t fileNo = count;
-    if(!counterFile)
-    {
-	cout << "WARNING: The vault file counter NOT found" <<endl;
-	return;
-    }
-    counterFile<<fileNo;
-    cout << "Counter set to: "<<fileNo <<endl;
-    counterFile.close();
-}
-
-void TStar::PrintCounter()
-{
-    TString filePath = Config->GetFilePath();
-    filePath += "FileCounter.txt";    
-    fstream counterFile(filePath, ios::in);
-    Int_t fileNo = -1;
-    if(!counterFile)
-    {
-	cout << "WARNING: The vault file-counter NOT found" <<endl;
-	return;
-    }
-
-    counterFile>>fileNo;	
-    counterFile.close();
-    cout << "File counter in the vault: "<< fileNo <<endl;
-}
-
-
-void TStar::FileViewer(Int_t fileNumber)
-{
-    //Open the file for reading
-    TString fileName = Config->GetFilePath() + "root/";
-    fileName += fileNumber;
-    fileName += ".root";
-    if(!IsValid(fileName))
-	return;
-    TFile *file = new TFile(fileName);
-    Int_t hist_counter = 0;
-
-    //Create an iterator
-    TIter nextkey (file->GetListOfKeys());
-    TKey *key;
-
-    while ( (key = (TKey*) nextkey() ))
-    {
-	//Read object from first source file
-	TObject *obj = key->ReadObj();
-
-	//Check that the object the key points to is a histogram
-	if (obj->IsA()->InheritsFrom("TH1") || obj->IsA()->InheritsFrom("TGraph"))
-	{
-	    //Cast the TObject pointer to a histogram one
-	    // TH1 * hist = (TH1*) (obj);
-	    
-	    TCanvas *c = new TCanvas();
-	    obj->Draw();
-	}
-	else if(obj->IsA()->InheritsFrom("TCanvas"))
-	    obj->Draw();
-    }    
-}
-
-void TStar::FileViewer(TString fileName)
+void TStar::view(TString fileName)
 {
     //Open the file for reading
     if(!IsValid(fileName))
@@ -187,7 +89,15 @@ void TStar::FileViewer(TString fileName)
 	TObject *obj = key->ReadObj();
 
 	//Check that the object the key points to is a histogram
-	if (obj->IsA()->InheritsFrom("TH1") || obj->IsA()->InheritsFrom("TGraph"))
+	if (obj->IsA()->InheritsFrom("TH2"))
+	{
+	    //Cast the TObject pointer to a histogram one
+	    // TH1 * hist = (TH1*) (obj);
+	    
+	    TCanvas *c = new TCanvas();
+	    obj->Draw("colz");
+	}
+	else if (obj->IsA()->InheritsFrom("TH1") || obj->IsA()->InheritsFrom("TGraph"))
 	{
 	    //Cast the TObject pointer to a histogram one
 	    // TH1 * hist = (TH1*) (obj);
@@ -200,70 +110,6 @@ void TStar::FileViewer(TString fileName)
     }    
 }
 
-void TStar::FilePrint(Int_t lowerRange, Int_t upperRange)
-{
-    for(int i = lowerRange; i <= upperRange; ++i)
-    {
-	//Open the file for reading
-	TString fileName = Config->GetFilePath() + "root/"; 
-	fileName += i;
-	fileName += ".root";
-	if(gSystem->AccessPathName(fileName))
-	    continue;
-	cout <<"\n============================================================================="<<endl;
-	cout << "\t\t\t File Number: "<<i<<endl;
-
-	cout <<"============================================================================="<<endl;
-	TFile *file = new TFile(fileName);
-	file->ls();
-	file->Close();
-    }
-}
-
-void TStar::FileListing(Int_t lowerRange, Int_t upperRange)
-{
-    for(int i = lowerRange; i <= upperRange; ++i)
-    {
-	//Open the file for reading
-	TString fileName = Config->GetFilePath() + "root/";
-	fileName += i;
-	fileName += ".root";
-	if(gSystem->AccessPathName(fileName))
-	    continue;
-	TFile *file = new TFile(fileName);
-	file->Print();
-	file->Close();
-    }
-}
-
-void TStar::FileListing()
-{
-    TString filePath = Config->GetFilePath();
-    filePath += "FileCounter.txt";    
-    fstream counterFile(filePath, ios::in);
-    Int_t fileNo = -1;
-    if(!counterFile)
-    {
-	cout << "WARNING: The file having the file-counter NOT found" <<endl;
-	return;
-    }
-
-    counterFile>>fileNo;	
-    counterFile.close();
-
-    for(int i = 0; i <= fileNo; ++i)
-    {
-	//Open the file for reading
-	TString fileName = Config->GetFilePath() + "root/";
-	fileName += i;
-	fileName += ".root";
-	if(gSystem->AccessPathName(fileName))
-	    continue;
-	TFile *file = new TFile(fileName);
-	file->Print();
-	file->Close();
-    }
-}
 
 
 void TStar::help()
@@ -453,4 +299,14 @@ void TStar::history()
     myFile.close();
 }
 
+void TStar::open(TString fileName)
+{
+    if(!IsValid(fileName))
+    {
+	cout<<"input file NOT found: "<< fileName<<endl;
+	return;
+    }
+    TFile *file = new TFile(fileName);
 
+    TBrowser *b = new TBrowser();    
+}
--- libBrStar/src/TStar.h
+++ libBrStar/src/TStar.h
@@ -24,18 +24,12 @@ public:
     static Int_t gFirstRun;   //This will make library loading slow (to read large json file)
     static Int_t gLastRun;
     
-    static Int_t GetCounter();
-    static void SetCounter(Int_t cout);
-    static void PrintCounter();
     static void Exit();
     static void ExitIfInvalid(TString filePath);
     static Bool_t IsValid(TString filePath);
-    static void FileViewer(Int_t fileNumber);
-    static void FileViewer(TString fileName);
-    static void FilePrint(Int_t lowerRange, Int_t upperRange);
-    static void FileListing(Int_t lowerRange, Int_t upperRange);
-    static void FileListing();
-
+    static void view(TString fileName);
+    static void open(TString fileName);
+    
     static void help();
     static void help(string file);
     static void history();
